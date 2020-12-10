4d478a3f87286af429d10ced7786f40cee4f60e7
Ajeya M Kempegowda <ajeya611b.94@gmail.com>
2020-12-10 11:11:01 -0500

    updated init.py by added numpy warning filters.
    a hack to make a local docker build pass 👍

--- sparse_dot_topn/__init__.py
+++ sparse_dot_topn/__init__.py
@@ -1,5 +1,11 @@
 # flake8: noqa
 import sys
+import warnings
+
+# Filter out Cython harmless warnings
+warnings.filterwarnings("ignore", message="numpy.dtype size changed")
+warnings.filterwarnings("ignore", message="numpy.ufunc size changed")
+warnings.filterwarnings("ignore", message="numpy.ndarray size changed")
 
 if sys.version_info[0] >= 3:
     from sparse_dot_topn.awesome_cossim_topn import awesome_cossim_topn
